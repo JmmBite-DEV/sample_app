99e215e518488248ed6b69145fc19f299e0f2331
maojieming <maojieming@gmail.com>
2014-06-08 12:13:56 +0800

    finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :password, length: { maximum: 8 }
+  validates :password, length: { maximum: 8, minimum: 6 }
 
-  before_validation { self.email = email.downcase; self.name = name.downcase }
+  # before_validation { self.email = email.downcase; self.name = name.downcase }
+  before_validation { email.downcase!; name.downcase! }
   validates :name, presence: true, length: { maximum: 16 }, uniqueness: true
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@([a-z\b\-]+.){1,}[a-z]+\z/i}, uniqueness: true
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  # Speed up tests by lowering bcrypt's cost function.
+  ActiveModel::SecurePassword.min_cost = true
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
