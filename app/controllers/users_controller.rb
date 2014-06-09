74bfa7f55dda4ddb23bb889f613c3787fa69accd
maojieming <maojieming@gmail.com>
2014-06-09 11:46:41 +0800

    finished sign-in-out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = 'Welcome to the Sample App!'
       redirect_to @user
     else
+      flash[:warning] = 'Login Failed!'
       render 'new'
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  before_create :create_remember_token
+
   has_secure_password
   validates :password, length: { maximum: 8, minimum: 6 }
 
@@ -6,4 +8,17 @@ class User < ActiveRecord::Base
   before_validation { email.downcase!; name.downcase! }
   validates :name, presence: true, length: { maximum: 16 }, uniqueness: true
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@([a-z\b\-]+.){1,}[a-z]+\z/i}, uniqueness: true
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
